56077b6a0a8890911d3b90d13f1f1de870ab12f7
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 19:59:43 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Combie_Destiny_1.1" `
+  -ModName "Combie Destiny 1.1" `
+  -ModFolder "CombineDestiny" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/combine-destiny/CombineDestiny-1.0+patch-1.1.zip" `
+  -ModBaseFilesUrlHash "2cc68fd71d1412e94d6ac36cfc48d4b0419dcafbb7a21ae20f9faa82a970a820" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
